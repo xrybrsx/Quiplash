c0ff48acec9a18a6f043a8ae58ea86e061992870
xrybrsx <55786389+xrybrsx@users.noreply.github.com>
2021-10-22 16:57:43 +0100

    reconstruction of functios

---
+++ register/__init__.py
@@ -0,0 +1,40 @@
+import logging
+import json
+import azure.functions as func
+from client import addPlayer
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info(
+        '----------------Python HTTP trigger function processed a request.----------------')
+    logging.info("The JSON received {}".format(req.get_json()))
+    username = req.params.get('username')
+
+    if not username:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            username = req_body.get('username')
+
+    if ((len(username) > 4) and (len(username) < 16)):
+        addPlayer(username)
+        return func.HttpResponse(f"Hello, {username}. Your profile was created.")
+    elif len(username) < 4:
+        json_msg = {"result": False,
+                    "msg": "Username less than 4 characters"}
+        return func.HttpResponse(json.dumps(json_msg),
+                                 status_code=400
+                                 )
+    elif len(username) > 16:
+        json_msg = {"result": False,
+                    "msg": "Username more than 16 characters"}
+        return func.HttpResponse(json.dumps(json_msg),
+                                 status_code=400
+                                 )
+    else:
+        return func.HttpResponse(
+            " Pass a name in the query string or in the request body for a personalized response.",
+            status_code=200
+        )
