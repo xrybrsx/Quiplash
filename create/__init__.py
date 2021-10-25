3df64a105827d489428b3b68cd69c634dc3410e2
xrybrsx <55786389+xrybrsx@users.noreply.github.com>
2021-10-25 19:31:44 +0100

    create function updated

--- create/__init__.py
+++ create/__init__.py
@@ -2,7 +2,8 @@ import logging
 import json
 from typing import Text
 import azure.functions as func
-from client import connectToContainer
+from client import connectToContainer, authorize
+import random
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
@@ -10,6 +11,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     username = req.params.get('username')
     text = req.params.get('text')
+    password = req.params.get('password')
     if (not username) or (not text):
         try:
             req_body = req.get_json()
@@ -32,29 +34,35 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             player.append(item)
 
         for item in containerPrompts.query_items(
-                query='SELECT * FROM prompts p WHERE p.text="{}"'.format(
-                    text),
+                query='SELECT * FROM prompts p WHERE p.text="{}" AND p.username="{}"'.format(
+                    text, username),
                 enable_cross_partition_query=True):
             logging.info(item)
             textExist.append(item)
 
     if (len(player) <= 0):
-        NoSuchUser = {"result": False, "msg": "user does not exist"}
-        return func.HttpResponse(json.dumps(NoSuchUser))
+        json_msg = {"result": False, "msg": "user does not exist"}
+        return func.HttpResponse(json.dumps(json_msg))
+    elif not authorize(username, password):
+        json_msg = {"result": False, "msg": "bad username or password"}
+        return func.HttpResponse(json.dumps(json_msg))
     elif (len(textExist)) > 0:
-        return func.HttpResponse("there is such text")
+        json_msg = {"result": False,
+                    "msg": "User already has a prompt with the same text"}
+        return func.HttpResponse(json.dumps(json_msg))
     elif (len(str(text)) < 10):
-        lessThan10 = {"result": False,
-                      "msg": "prompt is less than 10 characters"}
-        return func.HttpResponse(json.dumps(lessThan10))
+        json_msg = {"result": False,
+                    "msg": "prompt is less than 10 characters"}
+        return func.HttpResponse(json.dumps(json_msg))
     elif (len(str(text)) > 100):
-        moreThan100 = {"result": False,
-                       "msg": "prompt is more than 100 characters"}
-        return func.HttpResponse(json.dumps(moreThan100))
+        json_msg = {"result": False,
+                    "msg": "prompt is more than 100 characters"}
+        return func.HttpResponse(json.dumps(json_msg))
     else:
         new_prompt = containerPrompts.upsert_item({
             'text': '{}'.format(text),
-            'username': '{}'.format(username)
+            'username': '{}'.format(username),
+            'id': '{}'.format(random.randint(100, 1001))
         })
         return func.HttpResponse(
             json.dumps(new_prompt),
