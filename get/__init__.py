691d624e2c4f90b246333f154791df1f2223f83c
xrybrsx <55786389+xrybrsx@users.noreply.github.com>
2021-10-26 16:38:51 +0100

    get function added

--- get/__init__.py
+++ get/__init__.py
@@ -1,24 +1,39 @@
+import json
 import logging
-
 import azure.functions as func
+from client import connectToContainer
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
+    players = req.params.get('players')
+    if not players:
         try:
             req_body = req.get_json()
         except ValueError:
             pass
         else:
-            name = req_body.get('name')
+            name = req_body.get('players')
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    container = connectToContainer('quiplashDB', 'prompts')
+
+    if players == '-1':
+        item_list = list(container.read_all_items(max_item_count=10))
+        logging.info(item_list)
+        return func.HttpResponse(json.dumps(item_list))
+    elif players != '-1':
+        prompts = []
+        container = connectToContainer('quiplashDB', 'prompts')
+        for p in players:
+            if p.isnumeric():
+                for items in container.query_items(query='SELECT * FROM prompts p WHERE p.username="{}"'.format(
+                        p),
+                        enable_cross_partition_query=True):
+                    prompts.append(items)
+        return func.HttpResponse(json.dumps(prompts))
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
+            "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+            status_code=200
         )
